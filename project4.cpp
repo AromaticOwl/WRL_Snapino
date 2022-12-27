9e8eacd396f654da6ff3a9860036284cf1ded2e6
AromaticOwl <121521138+AromaticOwl@users.noreply.github.com>
2022-12-27 16:34:07 -0500

    Update project4.cpp

--- project4.cpp
+++ project4.cpp
@@ -6,3 +6,14 @@ void setup() {
   pinMode(9,OUTPUT);
   pinMode(10,OUTPUT);
 }
+
+void loop() {
+  digitalWrite(9, HIGH);
+  delay(delay1);
+  digitalWrite(9, LOW);
+  delay(delay1);
+  digitalWrite(10, HIGH);
+  delay(delay1);
+  digitalWrite(10, LOW);
+  delay(delay1);
+}
