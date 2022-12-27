744b815a7c06c36c6925777a5e3216319881c8cd
AromaticOwl <121521138+AromaticOwl@users.noreply.github.com>
2022-12-27 16:38:01 -0500

    Update project5.cpp

--- project5.cpp
+++ project5.cpp
@@ -1,1 +1,27 @@
+// Alternating_Lights
 
+int delay1 = 1000;
+int red = 11;
+int yellow = 10;
+int green = 9;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(green, HIGH);
+  delay(delay1);
+  
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  delay(delay1 / 2);
+  
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
+  delay(delay1);
+  
+  digitalWrite(red, LOW);
+}
